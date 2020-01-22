7022adcefc89a691868110b862a3c20fbfa47584
张中昊 <zhangzhonghao@tanwan.com>
2020-01-22 14:38:09 +0800

    A 增加数据库文件

---
+++ app/Admin/Controller/AutoReplyController.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/01/21
+ * Time: 10:44
+ */
+
+namespace App\Admin\Controller;
+
+class AutoReplyController extends BaseController
+{
+    public function textReplyList()
+    {
+
+    }
+
+    public function addTextReply()
+    {
+
+    }
+
+    public function updateTextReply()
+    {
+
+    }
+
+    public function giftCodeReplyList()
+    {
+
+    }
+
+    public function addGiftReply()
+    {
+
+    }
+
+    public function updateGiftReply()
+    {
+
+    }
+
+    public function pictureReplyList()
+    {
+
+    }
+
+    public function addPictureReply()
+    {
+
+    }
+
+    public function updatePictureReply()
+    {
+
+    }
+
+    public function soundReplyList()
+    {
+
+    }
+
+    public function addSoundReply()
+    {
+
+    }
+
+    public function updateSoundReply()
+    {
+
+    }
+
+    public function videoReplyList()
+    {
+
+    }
+
+    public function addVideoReply()
+    {
+
+    }
+
+    public function updateVideoReply()
+    {
+
+    }
+
+    public function articleReplyList()
+    {
+
+    }
+
+    public function addArticleReply()
+    {
+
+    }
+
+    public function updateArticleReply()
+    {
+
+    }
+
+    public function questionReplyList()
+    {
+
+    }
+
+    public function addQuestionReply()
+    {
+
+    }
+
+    public function updateQuestionReply()
+    {
+
+    }
+
+    public function buildReplyList()
+    {
+
+    }
+
+    public function addBuildReply()
+    {
+
+    }
+
+    public function updateBuildReply()
+    {
+
+    }
+}
--- sql/bananaWechatAdmin.sql
+++ sql/bananaWechatAdmin.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50728
  File Encoding         : 65001
 
- Date: 18/01/2020 19:35:11
+ Date: 22/01/2020 12:00:06
 */
 
 SET NAMES utf8mb4;
@@ -99,24 +99,157 @@ CREATE TABLE `bwa_admin_role_permission`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Table structure for bwa_auto_reply
+-- ----------------------------
+DROP TABLE IF EXISTS `bwa_auto_reply`;
+CREATE TABLE `bwa_auto_reply`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '唯一主键id',
+  `app_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '公众号的app_id',
+  `keyword_list` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '匹配出的关键字列表(关键字之间用逗号分隔)',
+  `event_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '匹配出后执行的event_key',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否可用:1可用；-1不可用',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for bwa_event_arbitrarily
+-- ----------------------------
+DROP TABLE IF EXISTS `bwa_event_arbitrarily`;
+CREATE TABLE `bwa_event_arbitrarily`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '唯一主键id',
+  `app_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '公众号app_id',
+  `reply_type` int(10) UNSIGNED NOT NULL DEFAULT 1 COMMENT '回复类型(1文本；2图片；3语音；4视频；5图文)',
+  `reply_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '回复内容的id',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否可用:1可用；-1不可用',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for bwa_event_build
+-- ----------------------------
+DROP TABLE IF EXISTS `bwa_event_build`;
+CREATE TABLE `bwa_event_build`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '唯一主键id',
+  `app_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '公众号app_id',
+  `event_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '事件标识',
+  `floor_type` int(10) NOT NULL COMMENT '盖楼获得奖品的楼层规则:1取模类型；2指定楼层类型',
+  `floor_num` int(10) NOT NULL COMMENT '盖楼获得奖品的楼层的取模=0的楼层（在取模类型可用）',
+  `floor_num_list` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '30' COMMENT '盖楼获得奖品的楼层列表（在指定楼层类型可用）',
+  `life_time` int(10) NOT NULL COMMENT '改一次楼能延续的时间',
+  `gift_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '获取奖品的奖品key',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否可用:1可用；-1不可用',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for bwa_event_follow
+-- ----------------------------
+DROP TABLE IF EXISTS `bwa_event_follow`;
+CREATE TABLE `bwa_event_follow`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '唯一主键id',
+  `app_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '公众号app_id',
+  `reply_type` int(10) UNSIGNED NOT NULL DEFAULT 1 COMMENT '回复类型(1文本；2图片；3语音；4视频；5图文)',
+  `reply_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '回复内容的id',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否可用:1可用；-1不可用',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for bwa_event_gift
+-- ----------------------------
+DROP TABLE IF EXISTS `bwa_event_gift`;
+CREATE TABLE `bwa_event_gift`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '唯一主键id',
+  `app_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '公众号app_id',
+  `event_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '事件标识',
+  `gift_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '获取奖品的奖品key',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否可用:1可用；-1不可用',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for bwa_event_keyword
+-- ----------------------------
+DROP TABLE IF EXISTS `bwa_event_keyword`;
+CREATE TABLE `bwa_event_keyword`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '唯一主键id',
+  `app_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '公众号app_id',
+  `event_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '事件标识',
+  `reply_type` int(10) UNSIGNED NOT NULL DEFAULT 1 COMMENT '回复类型(1文本；2图片；3语音；4视频；5图文)',
+  `reply_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '回复内容的id',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否可用:1可用；-1不可用',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for bwa_event_question
+-- ----------------------------
+DROP TABLE IF EXISTS `bwa_event_question`;
+CREATE TABLE `bwa_event_question`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '唯一主键id',
+  `app_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '公众号app_id',
+  `event_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '事件标识',
+  `question` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '问题内容',
+  `answer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '问题答案',
+  `life_time` int(10) UNSIGNED NOT NULL DEFAULT 30 COMMENT '回答限时(秒)，默认30秒',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否可用:1可用；-1不可用',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for bwa_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `bwa_menu`;
+CREATE TABLE `bwa_menu`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '唯一主键id',
+  `app_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '公众号appid',
+  `pid` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '菜单父级id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '菜单名称(此处需要有urlencode)',
+  `type` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '按钮点击触发的类型:view表示网页类型，click表示点击类型，miniprogram表示小程序类型，addSecondMenu增加二级菜单',
+  `view_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '跳转url:view类型必须',
+  `mp_app_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '小程序id:miniprogram类型必须',
+  `mp_page_path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '小程序地址:miniprogram类型必须',
+  `event_key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '按钮点击触发的自定义事件',
+  `weight` tinyint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '菜单权重的排序',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否可用:1可用；-1不可用',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
 -- Table structure for bwa_official_account
 -- ----------------------------
 DROP TABLE IF EXISTS `bwa_official_account`;
 CREATE TABLE `bwa_official_account`  (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '唯一主键id',
   `app_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '公众号appid',
-  `app_secret` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '公众号的appsecret',
-  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '推荐公众号名称',
-  `desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '说明',
-  `img` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '公众号图标',
+  `app_secret` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '公众号的appsecret',
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '推荐公众号名称',
+  `desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '说明',
+  `img` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '公众号图标地址',
   `platform_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '所属平台的id',
   `game_type_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '所属游戏类型的id',
   `game_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '所属游戏的id',
-  `account_username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `account_password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `update_time` int(10) UNSIGNED NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 0,
-  `create_time` int(10) UNSIGNED NOT NULL,
+  `account_username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '公众号账号',
+  `account_password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '公众号密码',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '可用情况：1可用；-1不可用',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
